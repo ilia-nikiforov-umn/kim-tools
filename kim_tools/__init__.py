7661cf8258bcd7c781ccbc8a50922710a0c76f84
ilia Nikiforov <nikif002@umn.edu>
2026-05-27 14:37:05 -0500

    test TODO

--- kim_tools/__init__.py
+++ kim_tools/__init__.py
@@ -11,4 +11,6 @@ from .test_driver import __all__ as test_driver_all
 from .vc import *
 from .vc import __all__ as vc_all
 
+# TODO: Collect all existing TODOS and make them issues
+
 __all__ = test_driver_all + aflow_all + symmetry_all + ase_all + vc_all
